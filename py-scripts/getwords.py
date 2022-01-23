f1449665e9560e70d54ac5d0bf97a580eb4b9b2c
Andrey Butenko <andreyxbutenko@gmail.com>
2022-01-22 21:57:40 -0800

    Use Wordle wordset

--- py-scripts/getwords.py
+++ py-scripts/getwords.py
@@ -1,22 +1,15 @@
 import base64
-from urllib.request import urlopen
 
-SOURCE_URL = 'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt'
+INPUT_FILE = './py-scripts/source.txt'
 OUTPUT_FILE = './words.b64.txt'
 
-# download file
-data = urlopen(SOURCE_URL).read()
-body = data.decode('utf-8')
-all_words = body.splitlines()
-
-# filter
-wordle_words = list(filter(lambda word: len(word) == 5, all_words))
-
-# serialize
-csv = ','.join(wordle_words)
+# read
+source = ''
+with open('py-scripts/wordle-wordset.txt', 'r') as f:
+  source = f.readline()
 
 # compress
-output = base64.b64encode(csv.encode('utf-8'))
+output = base64.b64encode(source.encode('utf-8'))
 
 # save
 with open(OUTPUT_FILE, 'wb') as file:
