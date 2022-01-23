4cda4ae7b03477ab08e30d074a86573949644cc7
Andrey Butenko <andreyxbutenko@gmail.com>
2022-01-23 12:50:57 -0800

    Attempt to fix broken symlinks on GH Pages

--- server.py
+++ server.py
@@ -11,9 +11,14 @@ ALLOWED_SYMLINK_DESTINATIONS = ['index.html']
 # http handler with symlink support
 class HttpRequestHandlerWithSymlinks(http.server.SimpleHTTPRequestHandler):
   def translate_path(self, req_path):
-    path = req_path[1:]
-    if path in ALLOWED_SYMLINK_SOURCES and os.path.islink(path) and os.readlink(path):
-      return os.readlink(path)
+    path = req_path
+    if path[0] == '/':
+      path = path[1:]
+    if path[-1] == '/':
+      path = path[:-1]
+    file_path = path + '/index.html'
+    if path in ALLOWED_SYMLINK_SOURCES and os.path.islink(file_path) and os.readlink(file_path):
+      return os.readlink(file_path)
     
     return super().translate_path(path)
 
